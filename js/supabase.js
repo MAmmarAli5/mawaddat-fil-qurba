6bdfd1e17d41063d37552b664e966d0921822b04
MAmmarAli5 <MawaddatfilQurba5@gmail.com>
2026-08-12 10:16:15 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,11 @@
+const SUPABASE_URL =
+    "https://vjrgmemaisxzjkmncvov.supabase.co";
 
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_9HmiXjkGNr-zAs2JUNzTlA_hXW8_DlE";
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
